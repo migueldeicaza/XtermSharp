d84abd3d6f565448a30af848dd58186992a3c090
Miguel de Icaza <miguel@gnome.org>
2019-11-19 15:28:29 -0500

    Fix a handful of style warnings, introduce CmdRet to be an alias for Carriage Return

--- XtermSharp/EscapeSequences.cs
+++ XtermSharp/EscapeSequences.cs
@@ -1,28 +1,29 @@
 ﻿using System;
 namespace XtermSharp {
 	public static class EscapeSequences {
-		public static byte[] CmdNewline = new byte[] { 10 };
-		public static byte[] CmdEsc = new byte[] { 0x1b };
-		public static byte[] CmdDel = new byte[] { 0x7f };
-		public static byte [] CmdDelKey = new byte [] { 0x1b, (byte) '[', (byte) '3', (byte) '~' };
-		public static byte[] MoveUpApp = new byte[] { 0x1b, (byte)'O', (byte)'A' };
-		public static byte[] MoveUpNormal = new byte[] { 0x1b, (byte)'[', (byte)'A' };
-		public static byte[] MoveDownApp = new byte[] { 0x1b, (byte)'O', (byte)'B' };
-		public static byte[] MoveDownNormal = new byte[] { 0x1b, (byte)'[', (byte)'B' };
-		public static byte[] MoveLeftApp = new byte[] { 0x1b, (byte)'O', (byte)'D' };
-		public static byte[] MoveLeftNormal = new byte[] { 0x1b, (byte)'[', (byte)'D' };
-		public static byte[] MoveRightApp = new byte[] { 0x1b, (byte)'O', (byte)'C' };
-		public static byte[] MoveRightNormal = new byte[] { 0x1b, (byte)'[', (byte)'C' };
-		public static byte[] MoveHomeApp = new byte[] { 0x1b, (byte)'O', (byte)'H' };
-		public static byte[] MoveHomeNormal = new byte[] { 0x1b, (byte)'[', (byte)'H' };
-		public static byte[] MoveEndApp = new byte[] { 0x1b, (byte)'O', (byte)'F' };
-		public static byte[] MoveEndNormal = new byte[] { 0x1b, (byte)'[', (byte)'F' };
-		public static byte[] CmdTab = new byte[] { 9 };
-		public static byte[] CmdBackTab = new byte[] { 0x1b, (byte)'[', (byte)'Z' };
-		public static byte[] CmdPageUp = new byte[] { 0x1b, (byte)'[', (byte)'5', (byte)'~' };
-		public static byte[] CmdPageDown = new byte[] { 0x1b, (byte)'[', (byte)'6', (byte)'~' };
+		public static byte[] CmdNewline = { 10 };
+		public static byte[] CmdRet = { 13 };
+		public static byte[] CmdEsc = { 0x1b };
+		public static byte[] CmdDel = { 0x7f };
+		public static byte[] CmdDelKey = { 0x1b, (byte) '[', (byte) '3', (byte) '~' };
+		public static byte[] MoveUpApp = { 0x1b, (byte)'O', (byte)'A' };
+		public static byte[] MoveUpNormal = { 0x1b, (byte)'[', (byte)'A' };
+		public static byte[] MoveDownApp = { 0x1b, (byte)'O', (byte)'B' };
+		public static byte[] MoveDownNormal = { 0x1b, (byte)'[', (byte)'B' };
+		public static byte[] MoveLeftApp = { 0x1b, (byte)'O', (byte)'D' };
+		public static byte[] MoveLeftNormal = { 0x1b, (byte)'[', (byte)'D' };
+		public static byte[] MoveRightApp = { 0x1b, (byte)'O', (byte)'C' };
+		public static byte[] MoveRightNormal = { 0x1b, (byte)'[', (byte)'C' };
+		public static byte[] MoveHomeApp = { 0x1b, (byte)'O', (byte)'H' };
+		public static byte[] MoveHomeNormal = { 0x1b, (byte)'[', (byte)'H' };
+		public static byte[] MoveEndApp = { 0x1b, (byte)'O', (byte)'F' };
+		public static byte[] MoveEndNormal = { 0x1b, (byte)'[', (byte)'F' };
+		public static byte[] CmdTab = { 9 };
+		public static byte[] CmdBackTab = { 0x1b, (byte)'[', (byte)'Z' };
+		public static byte[] CmdPageUp = { 0x1b, (byte)'[', (byte)'5', (byte)'~' };
+		public static byte[] CmdPageDown = { 0x1b, (byte)'[', (byte)'6', (byte)'~' };
 
-		public static byte[][] CmdF = new byte[][]{
+		public static byte[][] CmdF = {
 			new byte [] { 0x1b, (byte) 'O', (byte) 'P' }, /* F1 */
 			new byte [] { 0x1b, (byte) 'O', (byte) 'Q' }, /* F2 */
 			new byte [] { 0x1b, (byte) 'O', (byte) 'R' }, /* F3 */
